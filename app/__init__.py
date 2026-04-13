f8a175d1d5a8ebe97544ac09c874de578e7c51ae
RPM2626 <63054880+RPM2626@users.noreply.github.com>
2026-04-13 10:42:59 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,6 @@
-import random
-
-history = []
-secret_number = ""
-
-
-def generate_secret():
-    digits = list("0123456789")
-    random.shuffle(digits)
-    return "".join(digits[:4])
+from .models import *
+from .views import *
+from .controllers import *
+from .main import *
 
 
-def start_game():
-    global secret_number, history
-    secret_number = generate_secret()
-    history = []
-
-
-def evaluate_guess(guess):
-    global secret_number, history
-
-    bulls = 0
-    cows = 0
-
-    for i in range(4):
-        if guess[i] == secret_number[i]:
-            bulls += 1
-        elif guess[i] in secret_number:
-            cows += 1
-
-    result = {
-        "guess": guess,
-        "bulls": bulls,
-        "cows": cows
-    }
-
-    history.append(result)
-    return result
-
-
-def get_history():
-    return history
-
-
-start_game()
